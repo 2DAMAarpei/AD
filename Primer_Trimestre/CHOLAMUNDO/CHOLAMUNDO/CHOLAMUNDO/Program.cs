131f4e0e709336651540275fb856da2161c8f779
2DAMAarpei <aarpei@alu.iesserpis.org>
2019-09-20 09:40:17 +0200

    20/09/19

---
+++ Primer_Trimestre/CHOLAMUNDO/CHOLAMUNDO/CHOLAMUNDO/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CHOLAMUNDO
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
